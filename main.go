4b34ceafcc93c31483ee25be7b9712011f5609e6
Robin Schaefer <robin@schaefer.cm>
2023-06-24 12:18:37 +0200

    Added imports, converting macAddr to bin representation

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"encoding/hex"
+)
+
 func main() {
 
 	// Define macAddr, broadcastAddr to send network package to
 	const macAddr       = "6C1B9590FE32"
 	const broadCastAddr = "255.255.255.255"
+
+	// Convert macAddr in Bytes
+	mac, err := hex.DecodeString(macAddr)
+	if (err != nil) {
+		fmt.Println("Error converting (string) macAddr to byte representation")
+	}
+
 }
